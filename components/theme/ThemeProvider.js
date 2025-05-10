2d5346b7e5aaa747ffedf9fca5fca49b4249002a
Soumik Das <100500795+imsourobh@users.noreply.github.com>
2025-05-10 09:29:05 +0600

    Update ThemeProvider.js
    dark

--- components/theme/ThemeProvider.js
+++ components/theme/ThemeProvider.js
@@ -4,7 +4,7 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function ThemeProvider({ children, ...props }) {
   return (
-    <NextThemesProvider defaultTheme="dark" enableSystem={true} {...props}>
+    <NextThemesProvider defaultTheme="dark" enableSystem={false} {...props}>
       {children}
     </NextThemesProvider>
   );
